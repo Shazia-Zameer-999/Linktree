be9293748c349867570879e062fab1408b5cd5ed
Daten by SZ <datendiva7867@gmail.com>
2025-11-01 23:28:59 +0530

    faq debug 2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,12 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+    
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-img-element": "off",
+      "jsx-a11y/alt-text": "off",
+    },
   },
 ];
 
