0ff9857dbdb85151684937e6062debb8fe209b02
Daten by SZ <datendiva7867@gmail.com>
2026-07-05 15:59:53 +0530

    cart resolving

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import Navbar from "@/components/Navbar";
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL
   || (process.env.VERCEL_PROJECT_PRODUCTION_URL
     ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-    : "http://localhost:3000");
+    : "https://shop.datendiva.me");
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
