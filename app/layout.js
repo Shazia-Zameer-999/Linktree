4455cb75c23df6a4dea9172bf0e874cd3d4328c6
Daten by SZ <datendiva7867@gmail.com>
2026-07-05 16:05:07 +0530

    done

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import Navbar from "@/components/Navbar";
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL
   || (process.env.VERCEL_PROJECT_PRODUCTION_URL
     ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-    : "https://shop.datendiva.me");
+    : "http://localhost:3000");
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
